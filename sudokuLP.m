d601619d2e83712c85990197f02763c40161bc4c
Wallace O'Rear <worear@gmail.com>
2017-10-16 17:02:21 -0400

    added the set cell input

--- sudokuLP.m
+++ sudokuLP.m
@@ -4,7 +4,7 @@ f = (1:numVars);
 intcon = (1:numVars);
 ub=ones(numVars,1);lb=zeros(numVars,1);
 A=[];b=[];
-beq=ones(1,81*4); %add the set values to beq at end
+beq=ones(81*4,1); %add the set values to beq at end
 %vars
 %Xijk  i=row j=col k=intValueInCell
 %Xijk = 1 if value in cell i,j is = k
@@ -31,6 +31,28 @@ BoxCons=blkdiag(Z3,Z3,Z3);
 Aeq=[RowCons;ColCons;CellCons;BoxCons];
 
 %add on the set value constraints and beq rows
+vals = input('Enter the given cells in matrix format ( [row,col,value;])\n');
+
+[r,c] = size(vals);
+
+for row=1:1:r %loop through the given values and add to Aeq and beq
+    %easy part, add a 1 to the bottom of beq
+    beq=[beq;1];
+    %now the hard part, adding the 1 in the right cell of an 1x729 vector
+    %row=vals(row,1) col=vals(row,2) k=vals(row,3) assuming the user put the
+    %matrix in right
+    temp = zeros(1,729);
+    place=1;
+    for i=1:1:vals(row,1)
+        %place=place+1;
+        for j=1:1:vals(row,2)
+            place=place+9;
+        end
+    end
+    temp(place+vals(row,3)) = 1;
+    Aeq=[Aeq;temp];
+end
+
 
 %find out the solution
 z = intlinprog(f,intcon,A,b,Aeq,beq,lb,ub);
