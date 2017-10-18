c8b54b89d524023029e8ee11596b36ca5c886348
sooonia <kopels@g.cofc.edu>
2017-10-17 21:45:03 -0400

    Added Basis update
    Still buggy after an iteration

--- Simplex.m
+++ Simplex.m
@@ -2,7 +2,7 @@ A= [2 1 0 ; 0 0 1];
 b = [8 10]';
 f= [-1 -2 -2];
 
-function [z,x] = Simplex(f, A, b)
+function [z,x] = BabySimplex(f, A, b)
 %solve a linear minimization problem by using a simple variation of the
 %simplex algorithm
 %Solves min(f' * x) subject to A*x <= b
@@ -32,22 +32,39 @@ basic = [zeros(numNonBasic,1);ones(numBasic,1)];
 
 
 %--------------------------------------------------------------------------
-[optimal,deltaX] = getDeltaX(fnew, Anew, basic);
+[nbi,optimal,deltaX] = getDeltaX(fnew, Anew, basic);
 bounded = true;
 
 %Keep iterating until found unbounded or stop improving
 while(and(~optimal,bounded))
     %Get new X
+    deltaX
     lambda = getLambda(deltaX, xPrev);
     xNew = xPrev + lambda.*deltaX;
+    xNew
+    %Update Basis
+    tempBasis = basic;
+    tempBasis(nbi) = 1;
+    found = false;
+    i=0;
+    while and(~found, i<=length(basic))
+        i=i+1;
+        if and(basic(i)==1,xNew(i) == 0)
+            tempBasis(i) = 0;
+            found = true;
+      
+        end
+    end
+    basic = tempBasis;
     
-    %make sure still improving and bounded
+    %make sure bounded
     if(lambda == inf)
        bounded = false; 
     end
     
     %Set up for next iteration
     xPrev = xNew;
+    [nbi,optimal,deltaX] = getDeltaX(fnew, Anew, basic);
 end
 
 %return best Solution or print Unbounded
--- getDeltaX.m
+++ getDeltaX.m
@@ -1,6 +1,5 @@
-function [optimal, deltaX] = getDeltaX(fnew, Anew, basic)
+function [newBasicIdx, optimal, deltaX] = getDeltaX(fnew, Anew, basic)
    
-
 fBasic=[];
 ABasic=[];
 ANonBasic = [];
@@ -16,12 +15,13 @@ for i=1:1:length(basic)
        ANonBasic = [ANonBasic,Anew(:,i)];
    end   
 end
+ABasic
 v= fBasic * inv(ABasic);
 i=0;
 done = false;
 while and(~done,i<length(fNonBasic))
     i=i+1;
-   if (fNonBasic(i)-v * ANonBasic(:,i)) < 0
+   if (fNonBasic(i)-(v * ANonBasic(:,i))) < 0
        done = true;
    end
 end
@@ -29,21 +29,22 @@ end
 if done
     deltaXog = -1*inv(ABasic)*ANonBasic(:,i);
     k=1;
-    deltaX = zeros(length(Basic));
+    deltaX = zeros(length(basic),1);
+    newBasicIdx = i;
     for j = 1:length(basic)
         if j == i
-            deltaX(j) = 1;
-        elseif basic(j) == 1
-                deltaX(j) = deltaXog(k);
+            deltaX(j,1) = 1;
+        elseif basic(j,1) == 1
+                deltaX(j,1) = deltaXog(k);
                 k=k+1;
         else
-            deltaX(j) = 0;
+            deltaX(j,1) = 0;
         end
             
     end
     optimal = false;
 else
-    deltaX = null;
+    deltaX = zeros(length(basic));
     optimal = true;
 end
 
